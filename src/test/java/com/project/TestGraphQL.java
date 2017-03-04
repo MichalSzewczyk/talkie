281352c97f09f19065477d2c1ba0e768026b32bf
mszewczyk <szewczykm94@gmail.com>
2017-03-04 10:39:24 +0100

    Sample test for GraphQL

--- src/test/java/com/project/TestGraphQL.java
+++ src/test/java/com/project/TestGraphQL.java
@@ -3,20 +3,22 @@ package com.project;
 import com.project.graphql.test.GraphQLService;
 import com.project.graphql.test.Person;
 import graphql.GraphQL;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {GraphQLService.class, Person.class})
 public class TestGraphQL {
-    @Autowired
-    private GraphQLService graphQLService;
+    @Autowired()
+    private GraphQL graphQLService;
     @Test
     public void testGraphQLService(){
-        //TODO: getData throws null
-        System.out.println(new GraphQL(graphQLService.schema).execute("{id}").getData());
+        Assert.assertEquals("{id=0, name=SampleName, surname=SampleSurname}", graphQLService.execute("{id, name, surname}").getData().toString());
+        Assert.assertEquals("{id=0, name=SampleName, surname=SampleSurname}", graphQLService.execute("{id, name, surname}").getData().toString());
     }
 }
