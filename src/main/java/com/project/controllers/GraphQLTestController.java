caa780d3438f00bed85ff7437dbbcce698166056
mszewczyk <szewczykm94@gmail.com>
2017-03-05 18:56:38 +0100

    Controller logging

--- src/main/java/com/project/controllers/GraphQLTestController.java
+++ src/main/java/com/project/controllers/GraphQLTestController.java
@@ -1,6 +1,8 @@
 package com.project.controllers;
 
 import graphql.GraphQL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GraphQLTestController {
+    private final Logger logger = LoggerFactory.getLogger(GraphQLTestController.class);
     @Autowired
     private GraphQL graphQL;
 
     @RequestMapping("/graphQLTest")
     public String graphTest(@RequestParam(value = "id") String id) {
+        logger.info("Request with parameter: " + id);
         return (String) graphQL.execute(id).getData();
     }
 }
