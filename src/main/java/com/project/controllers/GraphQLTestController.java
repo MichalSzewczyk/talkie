031a86f2fca36f9a6f8fda581121d585cec6fc6c
Daniel Słaby <pufek93@gmail.com>
2017-03-05 19:30:08 +0100

    fixed request for graphql

--- src/main/java/com/project/controllers/GraphQLTestController.java
+++ src/main/java/com/project/controllers/GraphQLTestController.java
@@ -2,17 +2,19 @@ package com.project.controllers;
 
 import graphql.GraphQL;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GraphQLTestController {
     @Autowired
     private GraphQL graphQL;
 
     @RequestMapping("/graphQLTest")
-    public String graphTest(@RequestParam(value = "id") String id) {
-        return (String) graphQL.execute(id).getData();
+    public String graphTest(@RequestBody String query) {
+        return (String) graphQL.execute(query).getData();
     }
 }
