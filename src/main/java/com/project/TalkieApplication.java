2a6c54c6700b3384c5bb6e1dc245828f80cd0123
mszewczyk <szewczykm94@gmail.com>
2017-03-05 17:26:23 +0100

    Enabled Spring-boot autoconfiguration

--- src/main/java/com/project/TalkieApplication.java
+++ src/main/java/com/project/TalkieApplication.java
@@ -1,9 +1,13 @@
 package com.project;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan("com.project")
 public class TalkieApplication {
 
 	public static void main(String[] args) {
