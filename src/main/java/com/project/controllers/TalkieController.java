f9d35d817a5a73ca0f65f40a813e359c9dffdf62
mszewczyk <szewczykm94@gmail.com>
2017-03-12 17:06:43 +0100

    Applied changes to application controller

--- src/main/java/com/project/controllers/TalkieController.java
+++ src/main/java/com/project/controllers/TalkieController.java
@@ -1,7 +1,7 @@
 package com.project.controllers;
 
-import graphql.ExecutionResult;
-import graphql.GraphQL;
+import com.project.interfaces.WebExecutionResult;
+import com.project.interfaces.WebService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +11,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class GraphQLController {
-    private final Logger logger = LoggerFactory.getLogger(GraphQLController.class);
+public class TalkieController {
+    private final Logger logger = LoggerFactory.getLogger(TalkieController.class);
 
     private static final String REQUEST_INFO = "Request with parameter: %s";
     private static final String GRAPH_QL_ERROR = "Error: %s, while executing graphQl query for request body: %s";
     private static final String GRAPH_QL_RESULT = "Result of request: %s";
-
-    private final GraphQL graphQL;
+    private final WebService webService;
 
     @Autowired
-    public GraphQLController(GraphQL graphQL) {
-        this.graphQL = graphQL;
+    public TalkieController(WebService webService) {
+        this.webService = webService;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -31,7 +30,7 @@ public class GraphQLController {
     public Object graphTest(@RequestBody String requestBody) {
         logger.info(String.format(REQUEST_INFO, requestBody));
 
-        ExecutionResult result = graphQL.execute(requestBody);
+        WebExecutionResult result = webService.execute(requestBody);
 
         logger.info(String.format(GRAPH_QL_RESULT, result.getData()));
 
@@ -45,7 +44,7 @@ public class GraphQLController {
     @RequestMapping(value = "/sample")
     public String foo(@RequestBody String id) {
         logger.info("Request with parameter: " + id);
-        return (String) graphQL.execute(id).getData();
+        return (String) webService.execute(id).getData();
     }
 
 }
