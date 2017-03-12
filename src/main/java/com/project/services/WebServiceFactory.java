910c51df368230bae71ab7faf53d1a8c17ab97d3
mszewczyk <szewczykm94@gmail.com>
2017-03-12 16:55:58 +0100

    Created web service factory

---
+++ src/main/java/com/project/services/WebServiceFactory.java
@@ -0,0 +1,34 @@
+package com.project.services;
+
+import com.project.interfaces.WebService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebServiceFactory {
+    @Value("${webservice}")
+    private String webService;
+    @Lazy
+    @Autowired
+    private GraphQLService graphQLService;
+    @Lazy
+    @Autowired
+    private WebServiceOther webServiceOther;
+
+    @Primary
+    @Bean
+    WebService getWebService() {
+        switch (webService) {
+            case "graphQlService":
+                return graphQLService;
+            case "webServiceOther":
+                return webServiceOther;
+            default:
+                throw new UnsupportedOperationException("Not supported web service.");
+        }
+    }
+}
