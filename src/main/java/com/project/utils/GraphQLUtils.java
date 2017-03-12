1300465c1d40ed22195583992f1eeb8d406319df
mszewczyk <szewczykm94@gmail.com>
2017-03-12 11:38:14 +0100

    Implemented class with basic GraphQL utils

---
+++ src/main/java/com/project/utils/GraphQLUtils.java
@@ -0,0 +1,30 @@
+package com.project.utils;
+
+import graphql.schema.GraphQLArgument;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLScalarType;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static graphql.schema.GraphQLArgument.newArgument;
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
+
+@Component
+public final class GraphQLUtils {
+    public GraphQLFieldDefinition getFieldDefinition(String name, String description, GraphQLScalarType scalarType) {
+        return newFieldDefinition()
+                .name(name)
+                .description(description)
+                .type(scalarType).build();
+    }
+
+    public List<GraphQLArgument> getArgumentList(GraphQLScalarType scalarType, String... argNames) {
+        return Stream.of(argNames).map(argName -> newArgument()
+                .name(argName)
+                .type(scalarType)
+                .build()).collect(Collectors.toList());
+    }
+}
