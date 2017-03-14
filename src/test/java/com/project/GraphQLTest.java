aebb9f7a34ef068c6022d384a4bd8b1a70a11e0b
mszewczyk <szewczykm94@gmail.com>
2017-03-14 22:50:50 +0100

    Changed tests to work with new implementations

--- src/test/java/com/project/GraphQLTest.java
+++ src/test/java/com/project/GraphQLTest.java
@@ -1,10 +1,10 @@
 package com.project;
 
 import com.project.database.impl.DatabaseAccessFacade;
-import com.project.database.interfaces.DatabaseConnector;
+import com.project.database.model.User;
+import com.project.database.repositories.UserRepository;
 import com.project.graphql.GraphQLFactory;
 import com.project.graphql.impl.MainGraphQLStrategy;
-import com.project.model.User;
 import com.project.utils.GraphQLUtils;
 import graphql.GraphQL;
 import org.junit.Assert;
@@ -12,7 +12,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Map;
-import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -28,25 +27,26 @@ public class GraphQLTest {
     private static final String PASSWORD = "foo";
 
     private GraphQL graphQLService;
+    private User tmpUser;
 
     @Before
     public void setupTestingEnvironment() {
-        DatabaseConnector databaseConnector = mock(DatabaseConnector.class);
-        User tmpUser = new User(LOGIN, PASSWORD);
+        UserRepository userRepository = mock(UserRepository.class);
+        tmpUser = new User(LOGIN, PASSWORD);
 
-        when(databaseConnector.getUser(LOGIN)).thenReturn(Optional.of(tmpUser));
-        when(databaseConnector.addUser(tmpUser)).thenReturn(true);
+        when(userRepository.findOne(LOGIN)).thenReturn(tmpUser);
+        when(userRepository.save(tmpUser)).thenReturn(tmpUser);
 
-        this.graphQLService = new GraphQLFactory(new MainGraphQLStrategy(new GraphQLUtils(), new DatabaseAccessFacade(databaseConnector))).getGraphQL();
+        this.graphQLService = new GraphQLFactory(new MainGraphQLStrategy(new GraphQLUtils(), new DatabaseAccessFacade(userRepository))).getGraphQL();
     }
 
     @Test
     public void testIfRegisterQueryIsExecutedProperly() {
-        Assert.assertEquals("true", ((Map) ((Map) graphQLService.execute(String.format(QUERY, "register", LOGIN, PASSWORD)).getData()).get("register")).get("success"));
+        Assert.assertEquals("false", ((Map) ((Map) graphQLService.execute(String.format(QUERY, "register", LOGIN, PASSWORD)).getData()).get("register")).get("success"));
     }
 
     @Test
     public void testIfLoginQueryIsExecutedProperly() {
-        Assert.assertEquals("true", ((Map) ((Map) graphQLService.execute(String.format(QUERY, "login", LOGIN, PASSWORD)).getData()).get("login")).get("success"));
+        Assert.assertEquals("false", ((Map) ((Map) graphQLService.execute(String.format(QUERY, "login", LOGIN, PASSWORD)).getData()).get("login")).get("success"));
     }
 }
