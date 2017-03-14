2f7cfd8959c4282d7b76f1037cbd3ca658097fb7
mszewczyk <szewczykm94@gmail.com>
2017-03-14 22:42:22 +0100

    Created communication protocol factory

--- src/main/java/com/project/services/WebServiceFactory.java
+++ src/main/java/com/project/services/WebServiceFactory.java
@@ -4,11 +4,11 @@ import com.project.interfaces.WebService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
 
-@Service
+@Configuration
 public class WebServiceFactory {
     @Value("${webservice}")
     private String webService;
