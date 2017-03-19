b6cc4561fc4021ae813045f4107500f7e7c3f5cd
mszewczyk <szewczykm94@gmail.com>
2017-03-19 06:41:37 +0100

    Added searching by login to UserRepository

--- src/main/java/com/project/database/repositories/UserRepository.java
+++ src/main/java/com/project/database/repositories/UserRepository.java
@@ -1,12 +1,9 @@
 package com.project.database.repositories;
 
 import com.project.database.model.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+    User findOneByLogin(String login);
 
 }
