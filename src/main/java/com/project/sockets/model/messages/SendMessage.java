6fa4c6998fa07aa57c7093070714a1a298f7d59a
mszewczyk <szewczykm94@gmail.com>
2017-03-26 21:27:39 +0200

    Created SendMessage class

---
+++ src/main/java/com/project/sockets/model/messages/SendMessage.java
@@ -0,0 +1,37 @@
+package com.project.sockets.model.messages;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.project.sockets.model.messages.payloads.SendMessagePayload;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"type","payload"})
+public class SendMessage implements SocketMessage{
+
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("payload")
+    private SendMessagePayload payload;
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("payload")
+    public SendMessagePayload getPayload() {
+        return payload;
+    }
+
+    @JsonProperty("payload")
+    public void setPayload(SendMessagePayload payload) {
+        this.payload = payload;
+    }
+
+}
---
+++ src/main/java/com/project/sockets/model/messages/payloads/SendMessagePayload.java
@@ -0,0 +1,36 @@
+package com.project.sockets.model.messages.payloads;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"timestamp", "body"})
+public class SendMessagePayload {
+
+    @JsonProperty("timestamp")
+    private String timestamp;
+    @JsonProperty("body")
+    private String body;
+
+    @JsonProperty("timestamp")
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    @JsonProperty("timestamp")
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @JsonProperty("body")
+    public String getBody() {
+        return body;
+    }
+
+    @JsonProperty("body")
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
