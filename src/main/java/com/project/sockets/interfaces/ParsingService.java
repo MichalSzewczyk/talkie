06f0579270a6934bbce01c829063612022b2440a
mszewczyk <szewczykm94@gmail.com>
2017-03-27 23:30:08 +0200

    Changed return value

--- src/main/java/com/project/sockets/interfaces/ParsingService.java
+++ src/main/java/com/project/sockets/interfaces/ParsingService.java
@@ -1,10 +1,11 @@
 package com.project.sockets.interfaces;
 
+import com.project.sockets.model.MessageType;
 import com.project.sockets.model.messages.SocketMessage;
 import com.project.utils.Tuple;
 
 import java.io.IOException;
 
 public interface ParsingService {
-    Tuple<Object, Class<? extends SocketMessage>> parseSocketMessage(String json) throws IOException;
+    Tuple<Object, MessageType> parseSocketMessage(String json) throws IOException;
 }
