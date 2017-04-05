8b74cf4558f208f0f954f5a28ba8f37ecb56ecd1
mszewczyk <szewczykm94@gmail.com>
2017-04-05 07:11:17 +0200

    Allowed origins and removed unused properties

--- src/main/java/com/project/configuration/WebSocketConfig.java
+++ src/main/java/com/project/configuration/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(socketHandler, "/echo");
+        webSocketHandlerRegistry.addHandler(socketHandler, "/echo").setAllowedOrigins("*");
     }
 
 }
