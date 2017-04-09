21ef02e895a423e23b6bfd36401ef7ed0e615a2d
mszewczyk <szewczykm94@gmail.com>
2017-04-09 20:12:12 +0200

    Created abstract and concrete Visitors

---
+++ src/main/java/com/project/utils/HandlingVisitor.java
@@ -0,0 +1,16 @@
+package com.project.utils;
+
+import com.project.sockets.model.messages.requests.FetchUserStatus;
+import com.project.sockets.model.messages.requests.SendMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+
+public abstract class HandlingVisitor {
+    protected WebSocketSession session;
+    public abstract void visit(FetchUserStatus fetchUserStatus);
+    public abstract void visit(SendMessage fetchUserStatus);
+
+    public void setSession(WebSocketSession session) {
+        this.session = session;
+    }
+}
