1c7f42cdd64bf98e50fd4e6c8925e6a65d1c85e8
mszewczyk <szewczykm94@gmail.com>
2017-04-09 09:22:21 +0200

    Added support for USERS_STATUS

--- src/main/java/com/project/sockets/handlers/MessageHandlerFacade.java
+++ src/main/java/com/project/sockets/handlers/MessageHandlerFacade.java
@@ -5,6 +5,9 @@ import com.project.sockets.interfaces.ParsingService;
 import com.project.sockets.model.MessageType;
 import com.project.sockets.model.messages.requests.FetchUserStatus;
 import com.project.sockets.model.messages.requests.SendMessage;
+import com.project.sockets.model.messages.responses.FetchUsersStatusResponse;
+import com.project.sockets.model.payloads.FetchUsersResponsePayload;
+import com.project.sockets.model.payloads.UserElement;
 import com.project.utils.Tuple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,8 +20,12 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+
+import static com.project.sockets.model.MessageType.USERS_STATUS;
 
 @Component
 public class MessageHandlerFacade extends TextWebSocketHandler {
@@ -32,6 +39,8 @@ public class MessageHandlerFacade extends TextWebSocketHandler {
     private static final String ESTABLISHED_CONNECTION = "Established connection with user: %s";
     private static final String MESSAGE_SENT = "Message: %s sent to user with id: %s";
     private static final String HANDLING_MESSAGE = "Handling socket message: %s";
+    private static final String SERIALIZATION_FAILED = "Unable to serialize %s";
+    private static final String LOGGED_IN_USERS = "Users logged in: %s when user %s is logging in.";
 
     private ParsingService parsingService;
 
@@ -64,14 +73,27 @@ public class MessageHandlerFacade extends TextWebSocketHandler {
 
     private void handleFetchUserStatus(FetchUserStatus fetchUserStatus, WebSocketSession session) {
         List<String> friends = fetchUserStatus.getPayload().getListOfUsers();
-        friends.retainAll(loggedInUsers.keySet());
         Long userID = Long.parseLong(fetchUserStatus.getId());
         loggedInUsers.put(userID, session);
+        logger.info(String.format(LOGGED_IN_USERS, loggedInUsers, userID));
+        FetchUsersStatusResponse response = new FetchUsersStatusResponse(USERS_STATUS.toString(), new FetchUsersResponsePayload(prepareUserElements(friends, loggedInUsers.keySet())));
+        Optional<String> responseMessage = parsingService.serialize(response);
+
+        if(!responseMessage.isPresent()){
+            String errorInfo = String.format(SERIALIZATION_FAILED, USERS_STATUS);
+            logger.error(errorInfo);
+            throw new RuntimeException(errorInfo);
+        }
+
         try {
-            sendMessage(userID, "{\"type\":\"USERS_STATUS\",\"payload\":{\"users\":[{\"id\":\"1\", \"status\":\"1\" }]}}");
+            sendMessage(userID, responseMessage.get());
         } catch (IOException e) {
             //TODO: how should we solve lack of sender
         }
+    }
+
+    private UserElement[] prepareUserElements(List<String> friends, Set<Long> loggedIn){
+        return friends.stream().map(Long::parseLong).map(friendId -> new UserElement(String.valueOf(friendId), (loggedIn.contains(friendId))?"1":"0")).toArray(UserElement[]::new);
     }
 
     private void handleSendMessage(SendMessage sendMessage) {
@@ -89,7 +111,7 @@ public class MessageHandlerFacade extends TextWebSocketHandler {
         super.handleTextMessage(session, message);
         String plainTextMessage = message.getPayload();
         Tuple<Object, MessageType> result = parsingService.parseSocketMessage(plainTextMessage);
-        logger.info(HANDLING_MESSAGE, message);
+        logger.info(String.format(HANDLING_MESSAGE, message.getPayload()));
         switch (result.getValue()) {
             case FETCH_USER_STATUS:
                 handleFetchUserStatus((FetchUserStatus) result.getKey(), session);
