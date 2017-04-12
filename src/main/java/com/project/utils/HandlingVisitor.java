dc274cde8fb52f8e7825b3a87e7f9e5beb00001a
mszewczyk <szewczykm94@gmail.com>
2017-04-12 16:15:09 +0200

    Added handling to visitor pattern

--- src/main/java/com/project/utils/HandlingVisitor.java
+++ src/main/java/com/project/utils/HandlingVisitor.java
@@ -1,6 +1,7 @@
 package com.project.utils;
 
 import com.project.sockets.model.messages.requests.FetchUserStatus;
+import com.project.sockets.model.messages.requests.FindUser;
 import com.project.sockets.model.messages.requests.SendMessage;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -9,6 +10,7 @@ public abstract class HandlingVisitor {
     protected WebSocketSession session;
     public abstract void visit(FetchUserStatus fetchUserStatus);
     public abstract void visit(SendMessage fetchUserStatus);
+    public abstract void visit(FindUser findUser);
 
     public void setSession(WebSocketSession session) {
         this.session = session;
