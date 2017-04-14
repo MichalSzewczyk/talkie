308b1d1564ae777cfd9aa5da6ba89f4910dc364e
mszewczyk <szewczykm94@gmail.com>
2017-04-14 08:15:05 +0200

    Added basic documentation to  ParsingService

---
+++ talkie-dialect/src/main/java/com/talkie/dialect/parser/interfaces/ParsingService.java
@@ -0,0 +1,41 @@
+package com.talkie.dialect.parser.interfaces;
+
+import com.talkie.dialect.MessageType;
+import com.talkie.dialect.utils.Tuple;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Optional;
+
+
+
+public interface ParsingService {
+    /**
+     * Method is responsible for parsing json to socket messages
+     *
+     * @param  json is a json format of socket message to parse
+     * @return tuple where key is parsed socket message and value is
+     * type of that message according to MessageType enum.
+     *
+     */
+    Tuple<Object, MessageType> parseSocketMessage(String json) throws IOException;
+
+    /**
+     * Method is responsible for serializing object to json
+     *
+     * @param  object is object to serialize
+     * @return json representation of serialized object
+     *
+     */
+    Optional<String> serialize(Serializable object);
+
+    /**
+     * Method is responsible for deserialization object from json
+     *
+     * @param  json is string representation of serialized object
+     * @param  type is class to which we want to deserialize json
+     * @return deserialized object
+     *
+     */
+    <T> Optional<T> deserialize(String json, Class<T> type);
+}
