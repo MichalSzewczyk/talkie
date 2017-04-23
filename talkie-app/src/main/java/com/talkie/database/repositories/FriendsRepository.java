d7b5eba3b6b8197f8c2fdae0c1168e9fccbb667e
mszewczyk <szewczykm94@gmail.com>
2017-04-23 22:02:23 +0200

    Small fixes

--- talkie-app/src/main/java/com/talkie/database/repositories/FriendsRepository.java
+++ talkie-app/src/main/java/com/talkie/database/repositories/FriendsRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FriendsRepository extends CrudRepository<FriendRelation, Integer> {
-
+    FriendRelation findOneByWhoAndWith(Integer who, Integer with);
 }
